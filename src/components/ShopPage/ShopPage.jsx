2d82a07a2ed207a12bab9938d51e13493a9b6b0d
Dexter Crago <jodieanddexter@gmail.com>
2024-03-28 16:40:04 -0600

    Remove duplicate shopping cart component

--- src/components/ShopPage/ShopPage.jsx
+++ src/components/ShopPage/ShopPage.jsx
@@ -2,16 +2,14 @@ import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import NavBar from '../NavBar/NavBar';
 import ShopCategory from '../ShopCategory/ShopCategory';
-import ShoppingCart from '../ShoppingCart/ShoppingCart';
 import styles from './ShopPage.module.css';
-import shopCategories from '../../shop-categories';
+import shopCategories from '../../shop-information';
 
 function ShopPage() {
   return (
     <div className={styles.shop}>
       <Header />
       <NavBar />
-      <ShoppingCart />
       {shopCategories.map((category) => (
         <ShopCategory key={category} info={category} />
       ))}
