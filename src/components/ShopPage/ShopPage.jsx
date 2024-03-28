a8481a8f6eed9984b44f3d1a89a8aa6f0ec52466
Dexter Crago <jodieanddexter@gmail.com>
2024-03-28 15:13:09 -0600

    Add shopping cart component

--- src/components/ShopPage/ShopPage.jsx
+++ src/components/ShopPage/ShopPage.jsx
@@ -2,6 +2,7 @@ import Footer from '../Footer/Footer';
 import Header from '../Header/Header';
 import NavBar from '../NavBar/NavBar';
 import ShopCategory from '../ShopCategory/ShopCategory';
+import ShoppingCart from '../ShoppingCart/ShoppingCart';
 import styles from './ShopPage.module.css';
 
 function ShopPage() {
@@ -9,6 +10,7 @@ function ShopPage() {
     <div className={styles.shop}>
       <Header />
       <NavBar />
+      <ShoppingCart />
       <ShopCategory />
       <ShopCategory />
       <ShopCategory />
---
+++ src/components/ShoppingCart/ShoppingCart.jsx
@@ -0,0 +1,11 @@
+import styles from './ShoppingCart.module.css';
+
+function ShoppingCart() {
+  return (
+    <div>
+      <p className={styles.cart}>Shopping Cart</p>
+    </div>
+  );
+}
+
+export default ShoppingCart;
