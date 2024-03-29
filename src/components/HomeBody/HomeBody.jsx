2bdd58bacb6a12ef4a748568c8c8c95e7f30295a
Dexter Crago <jodieanddexter@gmail.com>
2024-03-29 13:37:06 -0600

    Add last two links on home page

--- src/components/HomeBody/HomeBody.jsx
+++ src/components/HomeBody/HomeBody.jsx
@@ -1,9 +1,13 @@
 import styles from './HomeBody.module.css';
+import { Link } from 'react-router-dom';
 
 function HomeBody() {
   return (
     <div>
       <p className={styles.text}>Body</p>
+      <Link to="/shop" className={styles.link}>
+        Shop
+      </Link>
     </div>
   );
 }
