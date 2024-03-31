1f4ecd47cd7ead0ee6ae75c6c561a6b72b005434
Dexter Crago <jodieanddexter@gmail.com>
2024-03-30 18:17:59 -0600

    Remove HomeBody/Trips components
    Their content was relocated into the HomePage component. It really just
    didn't seem like their content was worth having in seperate components.
    It will also be easier to add a background image to this portion of the
    webpage

--- src/components/HomePage/HomePage.jsx
+++ src/components/HomePage/HomePage.jsx
@@ -1,17 +1,24 @@
 import Header from '../Header/Header';
-import HomeBody from '../HomeBody/HomeBody';
+import { Link } from 'react-router-dom';
 import NavBar from '../NavBar/NavBar';
 import Footer from '../Footer/Footer';
 import styles from './HomePage.module.css';
-import HomeTrips from '../HomeTrips/HomeTrips';
 
 function HomePage() {
   return (
     <div className={styles.home}>
       <Header />
       <NavBar />
-      <HomeBody />
-      <HomeTrips />
+      <div>
+        <p className={styles.text}>Body</p>
+        <Link to="/shop" className={styles.link}>
+          Shop
+        </Link>
+      </div>
+      <div>
+        <p className={styles.trips}>Home Trips</p>
+        <Link>Trips</Link>
+      </div>
       <Footer />
     </div>
   );
